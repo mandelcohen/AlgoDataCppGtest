c35263b2a52159f6cb42ba0c93090263545b5247
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-07 11:56:17 +0200

    Update TurboSort header to take &args

--- src/class_library/TurboSort.h
+++ src/class_library/TurboSort.h
@@ -3,15 +3,69 @@
 #include <iostream>
 #include <cstdio>
 
-namespace TurboSort {
+namespace TurboSort
+{
+    class Sort
+    {
+        static int Partition(std::vector<int> &list, int low, int high)
+        {
+            int pivot = list[high];
+            int i = low;
+            for (int j = low; j <= high -1 ; ++j) {
+                if(list[j] < pivot)
+                {
+                    std::swap(list[j], list[i]);
+                    ++i;
+                }
+            }
+            std::swap(list[i], list[high]);
+            return i;
+        }
 
-    class TurboSort {
-        static void Swap(std::vector<int> list, int &a, int &b) {}
-        static void SelectionSort(std::vector<int> list){}
-        static void BubbleSort(std::vector<int> list){}
-        static int Partition(std::vector<int> list, int low, int high){}
-        static void QuickSort(std::vector<int> list, int low, int high){}
+    public:
+        static void SelectionSort(std::vector<int> &list){
+            int min;
+            for (int i = 0; i < list.size(); ++i)
+            {
+                min = i;
+                for (int j = i +1; j < list.size(); ++j)
+                {
+                    if(list[j] < list[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                if(min != i)
+                {
+                    std::swap(list[min], list[i]);
+                }
+            }
+        }
+
+        static void BubbleSort(std::vector<int> &list){
+            auto loop = list.size();
+            for (int i = loop -1; i > 0; --i) {
+                bool swapped = false;
+                for (int j = 0; j < i; ++j) {
+                    if(list[j] > list[j+1])
+                        std::swap(list[j], list[j+1]);
+                    swapped = true;
+                }
+                if(!swapped) break;
+            }
+        }
+
+
+        static void QuickSort(std::vector<int> &list, int low, int high)
+        {
+            if(low <= high)
+            {
+                auto partitionIndex = Partition(list, low, high);
+                QuickSort(list, low, partitionIndex -1);
+                QuickSort(list, partitionIndex +1, high);
+            }
+        }
     };
-
 }
 
