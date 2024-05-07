2daeb99c260fef24183d916af1b062c53195c43d
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-07 11:04:11 +0200

    Implement QuickSort and Partition

--- src/class_library/TurboSort.cpp
+++ src/class_library/TurboSort.cpp
@@ -27,12 +27,8 @@ namespace TurboSort {
                 Swap(list, min, i);
             }
         }
-
     }
-
-
-
-
+    
     static void BubbleSort(std::vector<int> list){
         auto loop = list.size();
         for (int i = loop -1; i > 0; --i) {
@@ -42,12 +38,36 @@ namespace TurboSort {
                     Swap(list, j, j+1);
                 swapped = true;
             }
-            if(!swapped)
+            if(!swapped) break;
         }
     }
 
+    static int Partition(std::vector<int> list, int low, int high)
+    {
+        int pivot = list[high];
+        int i = low;
+        for (int j = low; j <= high -1 ; ++j) {
+            if(list[j] < pivot)
+            {
+                Swap(list, j, i);
+                ++i;
+            }
+        }
+        Swap(list, i, high);
+        return i;
+    }
 
-    static void QuickSort(std::vector<int> list){}
+
+    static void QuickSort(std::vector<int> list, int low, int high)
+    {
+        if(low <= high)
+        {
+            auto partitionIndex = Partition(list, low, high);
+            QuickSort(list, low, partitionIndex -1);
+            QuickSort(list, partitionIndex +1, high);
+        }
+
+    }
 
 
 } // TurboSort
--- src/class_library/TurboSort.h
+++ src/class_library/TurboSort.h
@@ -7,10 +7,10 @@ namespace TurboSort {
 
     class TurboSort {
         static void Swap(std::vector<int> list, int &a, int &b) {}
-        static void SelectionSort(std::vector<int>){}
-        static void BubbleSort(std::vector<int>){}
-        static void QuickSort(std::vector<int>){}
-
+        static void SelectionSort(std::vector<int> list){}
+        static void BubbleSort(std::vector<int> list){}
+        static int Partition(std::vector<int> list, int low, int high){}
+        static void QuickSort(std::vector<int> list, int low, int high){}
     };
 
 }
