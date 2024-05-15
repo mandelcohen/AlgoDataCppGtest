40a68f0e90a3359a62ca4e339b324b46ddb7b882
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2024-05-15 13:43:39 +0200

    Add tests for BST

--- tests/class_library_tests/TurboBinarySearchTree.Tests.cpp
+++ tests/class_library_tests/TurboBinarySearchTree.Tests.cpp
@@ -1,3 +1,30 @@
-//
-// Created by Mandel Cohen on 2024-05-15.
-//
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "TurboBinarySearchTree.h"
+
+
+#define ALL_TESTS
+
+TEST(BST, TestInsertAndSearch)
+{
+    auto tree = new TurboBinarySearchTree<int>;
+
+    tree->Insert(4); tree->Insert(2); tree->Insert(6);
+    tree->Insert(1); tree->Insert(3); tree->Insert(5); tree->Insert(7);
+
+
+    ASSERT_TRUE(tree->Search(7));
+    ASSERT_FALSE(tree->Search(9));
+}
+
+TEST(BST, TestDeleteValueNoChild)
+{
+    auto tree = new TurboBinarySearchTree<int>;
+
+    tree->Insert(4); tree->Insert(2); tree->Insert(6);
+    tree->Insert(1); tree->Insert(3); tree->Insert(5); tree->Insert(7);
+
+    ASSERT_FALSE(tree->Delete(8));
+    ASSERT_TRUE(tree->Delete(7));
+
+}
